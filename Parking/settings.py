f89e1a4cfeb5af50e640479e4f41f8b2a44ab20f
RJey237 <rjavlon2004@gmail.com>
2025-04-08 19:20:27 +0500

    s3

--- Parking/settings.py
+++ Parking/settings.py
@@ -134,8 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT='static_root/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Add a static directory
 
+MEDIA_URL='media/'
+MEDIA_ROOT='media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
