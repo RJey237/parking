8d82545eaf88c7cf4607847a958b6ddfede48fd9
RJey237 <rjavlon2004@gmail.com>
2025-04-08 18:33:53 +0500

    setttings

--- Parking/urls.py
+++ Parking/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework_simplejwt.views import TokenBlacklistView
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apppark.api')),
@@ -29,3 +30,6 @@ urlpatterns = [
 
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
